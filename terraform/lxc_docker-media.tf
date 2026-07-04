770b99c232e346e771d458523c6316986c954ef5
furglitch <samsonger@proton.me>
2026-07-04 11:43:47 -0500

    fix: docker-media running out of memory and cpu

--- terraform/lxc_docker-media.tf
+++ terraform/lxc_docker-media.tf
@@ -1,79 +1,79 @@
 resource "proxmox_lxc" "docker_media" {
-    depends_on = [
-        proxmox_virtual_environment_download_file.template_debian_13,
+  depends_on = [
+    proxmox_virtual_environment_download_file.template_debian_13,
+  ]
+  ssh_public_keys = file(var.ssh_key_public)
+
+  hostname    = "docker-media"
+  vmid        = 221
+  target_node = var.pve_host_node
+  ostemplate  = "${var.image_template_prefix}${var.image_lxc_debian_13_file}"
+  tags        = "lxc;docker;media"
+  password    = var.user_password
+
+  start        = var.vm_boot_start
+  onboot       = var.vm_boot_start_onboot
+  unprivileged = var.lxc_unprivileged
+
+  cores  = 4
+  memory = 8192
+  swap   = var.hw_memory_swap
+
+  features {
+    fuse    = true
+    nesting = true
+    keyctl  = true
+    mount   = "ext4;nfs;cifs"
+  }
+
+  rootfs {
+    storage = var.storage_container
+    size    = "128G"
+  }
+
+  # mountpoint {
+  # 	key 			= "mp0"
+  # 	slot			= 0
+  # 	storage 		= var.share_mp_media
+  # 	mp				= var.share_mp_media
+  # 	size 			= "1G"
+  # 	backup 			= false
+  # }
+
+  network {
+    name   = var.network_name
+    bridge = var.network_bridge
+    ip     = "${var.network_ip_prefix}221/${var.network_cidr}"
+    gw     = var.network_gateway
+    ip6    = var.network_ipv6_type
+  }
+
+  lifecycle {
+    prevent_destroy = true
+
+    ignore_changes = [
+      ostemplate,
+      password,
+      ssh_public_keys,
+      rootfs,
+      swap,
+      tags,
     ]
-	ssh_public_keys     = file(var.ssh_key_public)
-
-	hostname	 		= "docker-media"
-	vmid         		= 221
-	target_node  		= var.pve_host_node
-	ostemplate   		= "${var.image_template_prefix}${var.image_lxc_debian_13_file}"
-	tags	   			= "lxc;docker;media"
-	password            = var.user_password
-
-	start	   			= var.vm_boot_start
-	onboot	   			= var.vm_boot_start_onboot
-	unprivileged 		= var.lxc_unprivileged
-
-	cores 				= 4
-	memory      		= 4096
-	swap        		= var.hw_memory_swap
-
-	features {
-		fuse            = true
-		nesting         = true
-		keyctl          = true
-		mount           = "ext4;nfs;cifs"
-	}
-
-	rootfs {
-		storage 		= var.storage_container
-		size 			= "128G"
-	}
-
-	# mountpoint {
-	# 	key 			= "mp0"
-	# 	slot			= 0
-	# 	storage 		= var.share_mp_media
-	# 	mp				= var.share_mp_media
-	# 	size 			= "1G"
-	# 	backup 			= false
-	# }
-
-	network {
-		name   			= var.network_name
-		bridge 			= var.network_bridge
-		ip     		    = "${var.network_ip_prefix}221/${var.network_cidr}"
-		gw		 		= var.network_gateway
-		ip6 			= var.network_ipv6_type
-	}
-
-	lifecycle {
-		prevent_destroy = true
-
-		ignore_changes = [
-			ostemplate,
-			password,
-			ssh_public_keys,
-			rootfs,
-			swap,
-			tags,
-		]
-	}
+  }
 }
 
 resource "null_resource" "docker_media_tun" {
-	depends_on = [
-		null_resource.truenas_share_mounts,
-	]
+  depends_on = [
+    null_resource.truenas_share_mounts,
+  ]
 
-	triggers = {
-		lxc_id = "221"
-		config_version = "2"
-	}
+  triggers = {
+    lxc_id         = "221"
+    config_version = "2"
+  }
 
-	provisioner "local-exec" {
-		command = <<-EOT
+  provisioner "local-exec" {
+    command = <<-EOT
 			echo "[[[PASSWORD ENTRY NEEDED]]]"
 
 			ssh -o StrictHostKeyChecking=no \
@@ -121,5 +121,5 @@ resource "null_resource" "docker_media_tun" {
 			pct reboot 221 || pct start 221
 ENDSSH
 		EOT
-	}
+  }
 }
